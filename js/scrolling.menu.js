15b6bcf17722a971ef41999d0153f683401542bf
kurupt5 <ricky.hopkins5@gmail.com>
2015-04-14 22:27:22 +1200

    Integrated some awesomeness

--- js/scrolling.menu.js
+++ js/scrolling.menu.js
@@ -1,5 +1,7 @@
 var scrollingMenu = $(".js-scrolling-menu"),
     journeyBtn = $(".js-journey"),
+    pageHeight = $(window).height(),
+    pageWidth = $(window).width(),
     pages = [
       "Title",
       "What we do",
@@ -32,6 +34,15 @@ function toggleFloats(index, nextIndex, direction){
   journeyBtn.css({ top: (nextIndex * 100) + "%"}).text(pages[index + 1]);
 }
 
+function leanJourneyBtn(e) {
+  var marginLeft = -75 + 50 * ((e.pageX / pageWidth) - 0.5),
+      marginTop = -50 - 80 * (e.pageY / pageHeight);
+  journeyBtn.css({"margin-left": marginLeft, "margin-top": marginTop});
+  console.log(marginTop);
+}
+
 journeyBtn.on("click", function() {
   $.fn.fullpage.moveSectionDown();
 });
+
+$("html").on("mousemove", leanJourneyBtn);
